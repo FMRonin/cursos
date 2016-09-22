1a623fa9e0b4f219f015b592bef76f8d8dfa23b0
Fabian Martinez <fabianemartinez@gmail.com>
2016-09-21 21:40:22 -0500

    Ejemplo DOM, Esqueleto HTML5, script.js

---
+++ HTML/script.js
@@ -0,0 +1,12 @@
+function ff() {
+	document.getElementById("pp").style="background-color:red"
+	// body...
+}
+function gg() {
+	document.getElementById("pp").style="background-color:yellow"
+	// body...
+}
+function hh() {
+	document.getElementById("in").style="background-color:yellow"
+	// body...
+}
